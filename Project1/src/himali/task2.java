da8855f7998d1eac185a0174d4432823af8f31cf
himali <himali@DESKTOP-D46JD1G>
2023-06-14 19:29:45 +0530

    changes made

--- Project1/src/himali/task2.java
+++ Project1/src/himali/task2.java
@@ -1,9 +1,10 @@
 package himali;
 
-public class task1 {
+public class task2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("update 1.0 ");
+		System.out.println("1st changes made ");
 	}
+
 }
