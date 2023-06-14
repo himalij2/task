d56d36a1cd091733488fa348e36c62cc1660c339
himali <himali@DESKTOP-D46JD1G>
2023-06-14 19:44:20 +0530

    lol

--- Project1/src/himali/task2.java
+++ Project1/src/himali/task2.java
@@ -4,6 +4,6 @@ public class task2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1st changes made ");
+		System.out.println("1st changes made");
 	}
 }
