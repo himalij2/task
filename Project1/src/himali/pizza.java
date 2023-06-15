a722808a7b89f237db95459c09bbd053251f7470
himali <himali@DESKTOP-D46JD1G>
2023-06-15 16:30:59 +0530

    pizza delivery check 1.0

---
+++ Project1/src/himali/pizza.java
@@ -0,0 +1,10 @@
+package himali;
+
+public class pizza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("your pizza is delivered successfully !");
+	}
+
+}
